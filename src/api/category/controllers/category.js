07af3105b7e17a986973cb3b907469a4dd537a3e
Sang Huynh Thanh <sanghuynh20000@gmail.com>
2024-03-18 13:57:34 +0700

    Update cútion categories

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -11,22 +11,30 @@ module.exports = createCoreController(
   ({ strapi }) => ({
     async find(ctx) {
       const categories = await super.find(ctx);
+      const limitProducts = 3;
 
       // Find first 3 products for each category, and total products
       await Promise.all(
         categories?.data?.map(async (category) => {
-          const products = await strapi.service("api::product.product").find({
-            category: category.id,
-            _limit: 3,
-            _sort: "createdAt:desc",
+          const products = await strapi.db.query("api::product.product").findMany({
+            where: {
+              categories: {
+                id: {
+                  $eq: category.id
+                }
+              }
+            },
+            populate: { thumbnail: true },
+            limit: limitProducts,
+            orderBy: { createdAt: 'desc' },
           });
 
           const totalProducts = await strapi.db.query("api::product.product").count({
             category: category.id,
           });
 
-          category.highlightProducts = products.results;
-          category.totalProducts = totalProducts;
+          category.attributes.highlightProducts = products;
+          category.attributes.totalProducts = totalProducts;
         })
       );
 
